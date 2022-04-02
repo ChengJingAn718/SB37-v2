821c8643de744271e594117dd03652845916c766
superdeveloper718 <96727045+superdeveloper718@users.noreply.github.com>
2022-04-02 12:22:01 -0700

    versoin 1.1

--- src/components/CommonVarariant.js
+++ src/components/CommonVarariant.js
@@ -412,7 +412,7 @@ const optionEyePosInfoList = [
 
 const optionCorrectAnswerList = [
     [2, 3, 1, 3, 1, 2, 1],
-    [2, 3, 2, 3, 1, 3, 1],
+    [2, 3, 1, 3, 1, 3, 1],
     [2, 1, 3, 1, 1, 3, 1],
     [1, 3, 2, 1, 1, 2, 3],
     [2, 1, 2, 1, 3, 2]
